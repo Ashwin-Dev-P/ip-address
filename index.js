01e873c3c1a9663f9dbfa5bc48d92e84c15dcb53
Ashwin-Dev-P <ashwindev1462001@gmail.com>
2022-07-05 19:18:21 +0530

    cors

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require("express");
+const cors = require("cors");
+
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
+
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 app.get("/", (req, res) => {
   /*
